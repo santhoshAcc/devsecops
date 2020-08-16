568fe7025f86beee1ae30dedf34fd62fc35655fa
Santhosh Reddy <Santhosh.dr.reddy@accenture.com>
2020-08-16 15:23:47 +0200

    Adding Jnkinsfile

--- src/main/java/com/example/devsecops/DevSecOpsApplication.java
+++ src/main/java/com/example/devsecops/DevSecOpsApplication.java
@@ -1,18 +1,13 @@
 package com.example.devsecops;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class DevSecOpsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevSecOpsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DevSecOpsApplication.class, args);
+    }
 
 }
